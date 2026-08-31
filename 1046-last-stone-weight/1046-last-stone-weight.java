3c15b83b161c76c0eae03d0c54dc9699919daa58
Shlok <156954411+Shlok2814@users.noreply.github.com>
2026-08-31 19:20:06 +0530

    Time: 2 ms (31.47%), Space: 42.8 MB (51.76%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -1,24 +1,11 @@
 class Solution {
     public int lastStoneWeight(int[] stones) {
-        int n = stones.length;
-        if(n == 1){
-            return stones[0];
-        }
         Arrays.sort(stones);
-        helper(stones, n - 1);
-
-        return stones[n-1];
-
-    }
-    private void helper(int[] arr, int i){
-        if(arr[i - 1] == 0){
-            return;
+        
+        for(int i = stones.length - 1; i > 0; i--){
+            stones[i - 1] = stones[i] - stones[i - 1];
+            Arrays.sort(stones);
         }
-
-        int diff = arr[i] - arr[i-1];
-        arr[i] = 0;
-        arr[i - 1] = diff;
-        Arrays.sort(arr);
-        helper(arr, i);
+        return stones[0];
     }
 }
